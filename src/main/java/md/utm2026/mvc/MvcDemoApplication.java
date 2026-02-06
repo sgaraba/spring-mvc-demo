e529075541dd633f1331fdcc1ff21cfdf0c664ea
Sergiu Garaba <sergiu.garaba@esempla.com>
2026-02-06 14:58:12 +0200

    First commit

---
+++ src/main/java/md/utm2026/mvc/MvcDemoApplication.java
@@ -0,0 +1,13 @@
+package md.utm2026.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcDemoApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(MvcDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/md/utm2026/mvc/MvcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package md.utm2026.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
